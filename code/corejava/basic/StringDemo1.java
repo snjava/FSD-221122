c2b1fd0366561ecde00d06364f0ecaf2e834deaf
scn-tech <scnaikwadi2@gmail.com>
2022-12-27 10:27:50 +0530

    String Demo
    String Demo

--- code/corejava/basic/StringDemo1.java
+++ code/corejava/basic/StringDemo1.java
@@ -6,11 +6,23 @@ public class StringDemo1
 		String str2 = new String("Hello");
 		String str3 = "Hello";
 		String str4 = "Hello";
+		String str5 = "HELLO";
 	
-		System.out.println(str1);
-		System.out.println(str2);
-		System.out.println(str3);
-		System.out.println(str4);
+		System.out.println(str1); // Hello
+		System.out.println(str2); // Hello
+		System.out.println(str3); // Hello
+		System.out.println(str4); // Hello
+
+		System.out.println(str1 == str2); // false
+		System.out.println(str1 == str3); // false
+		System.out.println(str3 == str4); // true 	
+
+		System.out.println(str1.equals(str2)); // true
+		System.out.println(str1.equals(str3)); // true
+		System.out.println(str3.equals(str4)); // true 	
+
+		System.out.println(str4 == str5); // false
+		System.out.println(str4.equalsIgnoreCase(str5)); // 	true			
 
 	}
 }
---
+++ code/corejava/basic/StringDemo2.java
@@ -0,0 +1,16 @@
+public class StringDemo2
+{
+	public static void main(String args[])
+	{
+		String firstName = "Abc";	
+		String lastName = "Xyz";
+		
+		String s1 = firstName.toUpperCase(); // ABC
+		lastName.toUpperCase();  // XYZ
+
+		System.out.println(firstName); // Abc
+		System.out.println(lastName);  // Xyz
+		System.out.println(s1); // ABC
+
+	}
+}
