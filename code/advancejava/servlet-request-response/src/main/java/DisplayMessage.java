47933a89c2082e1613bcad72377af3fa4c605b93
scn-tech <scnaikwadi2@gmail.com>
2023-03-12 10:10:04 +0530

    Request Response Handling
    Request Response Handling

--- code/advancejava/servlet-request-response/src/main/java/DisplayMessage.java
+++ code/advancejava/servlet-request-response/src/main/java/DisplayMessage.java
@@ -1,3 +1,5 @@
+
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -7,19 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/first-servlet-demo")
-public class FirstServlet extends HttpServlet {
+@WebServlet("/display-message")
+public class DisplayMessage extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Hello, This is my first servlet"); // display msg on console
-		
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		out.print("Hello This is the reponse"); // display the msg in the browser by returning response
-		out.print("<h1>Hello, this is HTML type of response</h1>");
 		
+		String name = request.getParameter("nm"); 
+		String message = request.getParameter("msg"); 
+		
+		out.print("<h1>Welcome User, '"+name+"'</h1>");
+		out.print("<h1>Your Message is '"+message+"'</h1>");
 	}
 }
-
-
-
-
--- code/advancejava/servlet-request-response/src/main/java/UserMessage.java
+++ code/advancejava/servlet-request-response/src/main/java/UserMessage.java
@@ -1,3 +1,5 @@
+
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -7,19 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/first-servlet-demo")
-public class FirstServlet extends HttpServlet {
+@WebServlet("/user-message")
+public class UserMessage extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Hello, This is my first servlet"); // display msg on console
-		
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		out.print("Hello This is the reponse"); // display the msg in the browser by returning response
-		out.print("<h1>Hello, this is HTML type of response</h1>");
-		
+		out.print("<h1>User Message</h1>");
+		out.print("<form action='display-message'>");
+			out.print("Enter Your Name: <input type='text' name='nm'>");
+			out.print("<br/><br/>");
+			out.print("Enter Your Message: <textarea rows='3' cols='15' name='msg' ></textarea>");
+			out.print("<br/><br/>");
+			out.print("<input type='Submit' value='Send Message'>");
+		out.print("</form>");
 	}
+
 }
-
-
-
-
