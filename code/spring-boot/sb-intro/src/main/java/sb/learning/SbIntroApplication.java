99ee04ff1cf43492448cc290734fe51afb5af154
snjava <scnaikwadi2@gmail.com>
2023-04-11 10:22:34 +0530

    Spring Boot Intro
    Spring Boot Intro

---
+++ code/spring-boot/sb-intro/src/main/java/sb/learning/SbIntroApplication.java
@@ -0,0 +1,13 @@
+package sb.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbIntroApplication.class, args);
+	}
+
+}
---
+++ code/spring-boot/sb-intro/src/test/java/sb/learning/SbIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
